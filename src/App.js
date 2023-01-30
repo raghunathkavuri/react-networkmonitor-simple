fdc1fce3c1770ce6e0472a0779f34ac224b219c2
raghunathkavuri <kavuri.raghu@gmail.com>
2023-01-30 16:11:28 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ export default function App() {
   const [numberOfPrices, setNumberOfPrices] = useState(10);
   const [difference, setDifference] = useState(50);
 
+  const strikeprices = [
+    16000, 16050, 16100, 16150, 16200, 16250, 16300, 16350, 16400, 16450, 16500,
+    16550, 16600, 16650, 16700, 16750, 16800, 16850, 16900, 16950, 17000, 17050,
+    17100, 17150, 17200, 17250, 17300, 17350, 17400, 17450, 17500, 17550, 17600,
+    17650, 17700, 17750, 17800, 17850, 17900, 17950, 18000, 18050, 18100, 18150,
+    18200, 18250, 18300, 18350, 18400, 18450, 18500, 18550, 18600, 18650, 18700,
+    18750, 18800, 18850, 18900, 18950, 19000, 19050, 19100, 19150, 19200, 19250,
+    19300, 19350, 19400, 19450, 19500, 19550, 19600, 19650, 19700, 19750, 19800,
+    19850, 19900, 19950, 20000, 20050, 20100, 20150, 20200, 20250, 20300, 21000,
+    22000, 23000, 24000,
+  ];
+
   return (
     <div>
       <label>Underlying Price:</label>
@@ -35,16 +47,17 @@ export default function App() {
         onChange={(e) => setDifference(e.target.value)}
       />
       <br />
-      <StrikePriceFilter
+      {/* <StrikePriceFilter
         underlyingPrice={underlyingPrice}
         numberOfPrices={numberOfPrices}
         difference={difference}
-      />
+      /> */}
 
       <StrikePricesFilter2
         underlyingPrice={underlyingPrice}
         numberOfPrices={numberOfPrices}
         difference={difference}
+        strikeprices={strikeprices}
       />
 
       <h3 class="ui center aligned icon header">
--- src/StrikePricesFilter2.js
+++ src/StrikePricesFilter2.js
@@ -6,23 +6,11 @@ const StrikePricesFilter2 = ({
   underlyingPrice,
   numberOfPrices,
   difference,
+  strikeprices,
 }) => {
   const [filteredPrices, setFilteredPrices] = useState([]);
 
-  const strikeprices = [
-    16000, 16050, 16100, 16150, 16200, 16250, 16300, 16350, 16400, 16450, 16500,
-    16550, 16600, 16650, 16700, 16750, 16800, 16850, 16900, 16950, 17000, 17050,
-    17100, 17150, 17200, 17250, 17300, 17350, 17400, 17450, 17500, 17550, 17600,
-    17650, 17700, 17750, 17800, 17850, 17900, 17950, 18000, 18050, 18100, 18150,
-    18200, 18250, 18300, 18350, 18400, 18450, 18500, 18550, 18600, 18650, 18700,
-    18750, 18800, 18850, 18900, 18950, 19000, 19050, 19100, 19150, 19200, 19250,
-    19300, 19350, 19400, 19450, 19500, 19550, 19600, 19650, 19700, 19750, 19800,
-    19850, 19900, 19950, 20000, 20050, 20100, 20150, 20200, 20250, 20300, 21000,
-    22000, 23000, 24000,
-  ];
-
   useEffect(() => {
-
     const closestPrice = strikeprices.reduce((prev, curr) =>
       Math.abs(curr - underlyingPrice) < Math.abs(prev - underlyingPrice)
         ? curr
@@ -39,14 +27,19 @@ const StrikePricesFilter2 = ({
           index === 0 || price - arr[index - 1] === difference
       );
     setFilteredPrices(filteredPrices);
-  }, [underlyingPrice, numberOfPrices, difference]);
+  }, [underlyingPrice, numberOfPrices, difference, strikeprices]);
 
   return (
     <div>
-      <p>This component filters strike prices before and after the underlying price </p>
+      <p>
+        This component filters strike prices before and after the underlying
+        price{' '}
+      </p>
       <ul>
         {filteredPrices.map((price, index) => (
-          <li key={index}>{index + 1}) {price}</li>
+          <li key={index}>
+            {index + 1}) {price}
+          </li>
         ))}
       </ul>
     </div>
