60a53c95566db397dbfe3adad386ed94ce237a3e
raghunathkavuri <kavuri.raghu@gmail.com>
2023-01-30 16:06:43 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 import useOnline from './onlinestatus';
 import StrikePriceFilter from './StrikePriceFilter';
+import StrikePricesFilter2 from './StrikePricesFilter2';
 
 export default function App() {
   const online = useOnline();
 
+  const [underlyingPrice, setUnderlyingPrice] = useState(17456);
+  const [numberOfPrices, setNumberOfPrices] = useState(10);
+  const [difference, setDifference] = useState(50);
+
   return (
     <div>
-      <div>
-        <StrikePriceFilter />
-      </div>
+      <label>Underlying Price:</label>
+      <input
+        type="number"
+        value={underlyingPrice}
+        onChange={(e) => setUnderlyingPrice(e.target.value)}
+      />
+      <br />
+      <label>Number of Prices:</label>
+      <input
+        type="number"
+        value={numberOfPrices}
+        onChange={(e) => setNumberOfPrices(e.target.value)}
+      />
+      <br />
+      <label>Difference:</label>
+      <input
+        type="number"
+        value={difference}
+        onChange={(e) => setDifference(e.target.value)}
+      />
+      <br />
+      <StrikePriceFilter
+        underlyingPrice={underlyingPrice}
+        numberOfPrices={numberOfPrices}
+        difference={difference}
+      />
+
+      <StrikePricesFilter2
+        underlyingPrice={underlyingPrice}
+        numberOfPrices={numberOfPrices}
+        difference={difference}
+      />
 
       <h3 class="ui center aligned icon header">
         <i class="circular plug icon"></i>
